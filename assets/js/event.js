9320ecb91ba7f689c3c211b8a7e7da41f03efb9c
marouan rtima <marouanrtima5@gmail.com>
2023-05-12 16:04:36 +0100

    i fixed a wrapper_side_bar for mobile and i add icon xmark for hidden class side-bar

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,8 +1,14 @@
 const toggleMenu = document.getElementsByClassName("toggle-menu")[0];
 
 const wrapperContainer = document.getElementById("wrapper_side_bar");
+const xmark = document.getElementById("xmark");
 
 toggleMenu.addEventListener("click", (eo) => {
     eo.preventDefault();
     wrapperContainer.classList.toggle("side-bar");
 });
+
+xmark.addEventListener("click", (eo) => {
+    eo.preventDefault();
+    wrapperContainer.classList.remove("side-bar");
+})
