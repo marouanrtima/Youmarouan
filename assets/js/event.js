cb3b4be1d73355f2d0aa14eefbd4832050953ae6
marouan rtima <marouanrtima5@gmail.com>
2023-05-18 23:10:57 +0100

    i finished write html and css section random-question-container

--- assets/js/event.js
+++ assets/js/event.js
@@ -4,13 +4,26 @@ const wrapperContainer = document.getElementById("wrapper_side_bar");
 const xmark = document.getElementById("xmark");
 
 toggleMenu.addEventListener("click", (eo) => {
-    eo.preventDefault();
-    wrapperContainer.classList.toggle("side-bar");
+  eo.preventDefault();
+  wrapperContainer.classList.toggle("side-bar");
 });
 
 xmark.addEventListener("click", (eo) => {
-    eo.preventDefault();
-    wrapperContainer.classList.remove("side-bar");
+  eo.preventDefault();
+  wrapperContainer.classList.remove("side-bar");
+});
+
+const randomQuestions = document.getElementById("random-questions");
+console.log("xoxo");
+
+const innerQuestion = document.querySelector(".inner-question");
+
+randomQuestions.style.border = "5px solid red";
+
+
+randomQuestions.addEventListener("click" , (eo) => {
+  if(eo.target.classList == "inner-question"){
+    eo.target.getElementsByClassName("content")[0].classList.toggle("show-content");
+  }
+
 })
-
-
