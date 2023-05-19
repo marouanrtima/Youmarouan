3cf973e5fe3d396406fd8c135cb94df2b25c3b1c
marouan rtima <marouanrtima5@gmail.com>
2023-05-19 12:25:40 +0100

    started coding footer

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,29 +1,25 @@
 const toggleMenu = document.getElementsByClassName("toggle-menu")[0];
 
 const wrapperContainer = document.getElementById("wrapper_side_bar");
-const xmark = document.getElementById("xmark");
+const xMark = document.getElementById("xmark");
 
 toggleMenu.addEventListener("click", (eo) => {
   eo.preventDefault();
   wrapperContainer.classList.toggle("side-bar");
 });
 
-xmark.addEventListener("click", (eo) => {
+xMark.addEventListener("click", (eo) => {
   eo.preventDefault();
   wrapperContainer.classList.remove("side-bar");
 });
 
-const randomQuestions = document.getElementById("random-questions");
-console.log("xoxo");
-
-const innerQuestion = document.querySelector(".inner-question");
-
-randomQuestions.style.border = "5px solid red";
-
-
-randomQuestions.addEventListener("click" , (eo) => {
-  if(eo.target.classList == "inner-question"){
-    eo.target.getElementsByClassName("content")[0].classList.toggle("show-content");
-  }
-
-})
+// const randomQuestions = document.getElementById("random-questions");
+// const innerQuestion = document.querySelector("inner-question");
+// randomQuestions.addEventListener("click", (eo) => {
+//   if (eo.target.classList == "inner-question") {
+//     eo.target.style.border = "5px solid red";
+//     eo.target
+//       .getElementsByClassName("content")[0]
+//       .classList.toggle("show-content");
+//   }
+// });
