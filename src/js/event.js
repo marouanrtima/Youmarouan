a5abb922d7f7a1858d087cbd45cb9647e89a8a8c
marouan rtima <marouanrtima5@gmail.com>
2023-05-22 12:56:02 +0100

    fixing dropdown-language with js

--- src/js/event.js
+++ src/js/event.js
@@ -1,3 +1,11 @@
+const dropdownLanguage = document.querySelector(".dropdown-wrapper");
+console.log(dropdownLanguage);
+
+dropdownLanguage.addEventListener("click", (eo) => {
+  console.log("done");
+  eo.target.classList.toggle("show-dropdown");
+});
+
 const toggleMenu = document.getElementsByClassName("toggle-menu")[0];
 
 const wrapperContainer = document.getElementById("wrapper_side_bar");
@@ -13,6 +21,19 @@ xMark.addEventListener("click", (eo) => {
   wrapperContainer.classList.remove("side-bar");
 });
 
+// for scroll bar
+const headerContainer = document.querySelector(".header-container");
+window.addEventListener("scroll", (eo) => {
+  let scrollTop = document.documentElement.scrollTop;
+  if (scrollTop === 0) {
+    headerContainer.classList.remove("scroll");
+    // console.log("close");
+  } else if (scrollTop >= 0) {
+    headerContainer.classList.add("scroll");
+    // console.log("done");
+  }
+});
+
 // const randomShow = document.getElementById("random-questions");
 
 const innerQuestion = document.querySelectorAll(".inner-question");
@@ -21,20 +42,4 @@ innerQuestion.forEach((item) => {
   item.addEventListener("click", () => {
     item.classList.toggle("show-content");
   });
-});
-
-
-
-// for scroll bar
-
-const headerContainer = document.querySelector(".header-container");
-window.addEventListener("scroll", (eo) => {
-  let scrollTop = document.documentElement.scrollTop;
-  if (scrollTop === 0) {
-    headerContainer.classList.remove("scroll")
-    // console.log("close");
-  } else if (scrollTop >= 0) {
-    headerContainer.classList.add("scroll")
-    // console.log("done");
-  }
 });
